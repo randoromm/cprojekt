2c7271c71d2902ae28176ab3df1b17aa83aa8e88
Rando R <rando_rommot@live.com>
2017-10-09 17:27:53 +0300

    Lab1 code finished.

--- src/main.c
+++ src/main.c
@@ -1,30 +1,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BLINK_DELAY_MS_1 3000
-#define BLINK_DELAY_MS_2 2000
-#define BLINK_DELAY_MS_3 200
-#define BLINK_DELAY_MS_4 10
+#define BLINK_DELAY_MS_1 1000
 
-void main (void)
-{
-    /* Set port B pin 7 for output for Arduino Mega yellow LED */
+void main (void) {
+    /* Set port B pin 7 for output for Arduino Mega green LED */
     DDRB |= _BV(DDB7);
+    /* Set port B pin 6 for output for Arduino Mega red LED */
     DDRB |= _BV(DDB6);
 
     while (1) {
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED on */
-        PORTB |= _BV(PORTB7);
-        _delay_ms(BLINK_DELAY_MS_3);
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED off */
-        PORTB &= ~_BV(PORTB7);
-        _delay_ms(BLINK_DELAY_MS_4);
-
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED on */
-        PORTB |= _BV(PORTB6);
-        _delay_ms(BLINK_DELAY_MS_3);
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED off */
-        PORTB &= ~_BV(PORTB6);
-        _delay_ms(BLINK_DELAY_MS_4);
+        /* Set port B pin 7 high to turn Arduino Mega green and red (yellow) LED on */
+        PORTB |= _BV(PORTB7) | _BV(PORTB6);
+        _delay_ms(BLINK_DELAY_MS_1);
+        /* Set port B pin 7 high to turn Arduino Mega green and red (yellow) LED off */
+        PORTB &= ~(_BV(PORTB7) | _BV(PORTB6));
+        _delay_ms(BLINK_DELAY_MS_1);
     }
 }
--- tooling/format-code.sh
+++ tooling/format-code.sh
@@ -17,7 +17,7 @@ fi
 # For all files provided as arguments.
 for FILE in "$@"
 do
-    RESULT="$(astyle    --style=1tbs \
+    RESULT="$(astyle    --style=java \
                         --indent-col1-comments \
                         --break-blocks \
                         --pad-oper \
