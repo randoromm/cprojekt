dfb9ea9d818521f8b7071292518b1842b47a5c8b
Rando R <rando_rommot@live.com>
2018-01-16 11:43:41 +0200

    Lab03 UART console finished.

--- lib/matejx_avr_lib/hwdefs_minimal.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAT_HWDEFS_H
-#define MAT_HWDEFS_H
-
-	#define DDR(x) (*(&x - 1))
-	#define PIN(x) (*(&x - 2))
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,20 +1,62 @@
+#include <stdio.h>
+#define __ASSERT_USE_STDERR
+#include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include "uart.h"
+#define BLINK_DELAY_MS_1 100
 
-#define BLINK_DELAY_MS_1 1000
+static inline void init_leds(void) {
+    /* Set port A pin 0 for output for Arduino Mega red LED */
+    DDRA |= _BV(DDA0);
+}
 
-void main (void) {
-    /* Set port B pin 7 for output for Arduino Mega green LED */
-    DDRB |= _BV(DDB7);
-    /* Set port B pin 6 for output for Arduino Mega red LED */
-    DDRB |= _BV(DDB6);
+
+/* Init error console as stderr in UART1 and print user code info */
+static inline void init_errcon(void) {
+    simple_uart1_init();
+    stderr = &simple_uart1_out;
+    fprintf(stderr, "Version: %s built on: %s %s\n",
+            FW_VERSION, __DATE__, __TIME__);
+    fprintf(stderr, "avr-libc version: %s avr-gcc version: %s\n",
+            __AVR_LIBC_VERSION_STRING__, __VERSION__);
+}
+
+
+static inline void blink_leds(void) {
+    /* Set port A pin 0 high to turn Arduino Mega red LED on */
+    PORTA |= _BV(PORTA0);
+    _delay_ms(BLINK_DELAY_MS_1);
+    /* Set port B pin 0 high to turn Arduino Mega red LED off */
+    PORTA &= ~_BV(PORTA0);
+    _delay_ms(BLINK_DELAY_MS_1);
+}
+
+
+void main(void) {
+    init_leds();
+    init_errcon();
+    /* Test assert - REMOVE IN FUTURE LABS */
+    char *array;
+    uint32_t i = 1;
+    extern int __heap_start, *__brkval;
+    int v;
+    array = malloc( i * sizeof(char));
+    assert(array);
+    /* End test assert */
 
     while (1) {
-        /* Set port B pin 7 high to turn Arduino Mega green and red (yellow) LED on */
-        PORTB |= _BV(PORTB7) | _BV(PORTB6);
-        _delay_ms(BLINK_DELAY_MS_1);
-        /* Set port B pin 7 high to turn Arduino Mega green and red (yellow) LED off */
-        PORTB &= ~(_BV(PORTB7) | _BV(PORTB6));
-        _delay_ms(BLINK_DELAY_MS_1);
+        blink_leds();
+        /* Test assert - REMOVE IN FUTURE LABS */
+        /*
+         * Increase memory allocated for array by 100 chars
+         * until we have eaten it all and print space between stack and heap.
+         * That is how assert works in run-time.
+         */
+        array = realloc( array, (i++ * 100) * sizeof(char));
+        fprintf(stderr, "%d\n",
+                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
+        assert(array);
+        /* End test assert */
     }
 }
