d5dee0dbbf6786c7f2724d200df905e8254ed927
Rando R <rando_rommot@live.com>
2018-01-17 18:04:06 +0200

    Lab04 finished. LCD problem solved

--- src/main.c
+++ src/main.c
@@ -5,13 +5,13 @@
 #include "uart.h"
 #include "print_helper.h"
 #include "hmi_msg.h"
+#include "../lib/hd44780_111/hd44780.h"
 
 static inline void init_leds(void) {
     /* Set port A pin 0 for output for Arduino Mega red LED */
     DDRA |= _BV(DDA0);
 }
 
-/* Init error console as stderr in UART1 and print user code info */
 static inline void init_errcon(void) {
     simple_uart1_init();
     simple_uart0_init();
@@ -38,12 +38,16 @@ static inline void ask_number(void) {
     fscanf(stdin, "%c", &inBuffer);
     fprintf(stdout, "%c\n", inBuffer);
     int number = (int)inBuffer - (int)'0';
+    lcd_goto(0x40);
 
     if (number >= 0 && number < 10) {
         fprintf_P(stdout, PSTR(ACK_NUMBER "\n"),
-                  (PGM_P)pgm_read_word(&(num_strings[number])));
+                  (PGM_P)pgm_read_word(&num_strings[number]));
+        lcd_puts_P((PGM_P)pgm_read_word(&num_strings[number]));
+        lcd_puts_P(PSTR("       "));
     } else {
         fprintf_P(stdout, PSTR(INC_NUMBER "\n"));
+        lcd_clr(0x40, 0x16);
     }
 }
 
@@ -61,6 +65,9 @@ static inline void blink_leds(void) {
 void main(void) {
     init_leds();
     init_errcon();
+    lcd_init();
+    lcd_clrscr();
+    lcd_puts_P(PSTR(NAME_STRING));
 
     while (1) {
         blink_leds();
